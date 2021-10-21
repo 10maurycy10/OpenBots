f15157f685a12b3b6dea80c43b3be0777a64a433
Maurycy Z <10maurycy10@gmail.com>
2021-10-21 09:27:46 -0700

    config fixes

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ config = {
     WEB_SERVER: false,
     MOVE_RANDOM_WALK_TIME: 500,
     DONT_AIM: false,
-    ATTACK_SELF: false,
+    DONT_ATTACK_SELF: true,,
     // the is how mutch the bot will reotate per second
     ARROWING_ANGULAR_SPEED: 2.9,
     SERVER_TICKS_MS: 16
